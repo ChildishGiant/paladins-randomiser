26e093514d941e965b3cfe26db885abbe1f89e01
ChildishGiant <ChildishGiant@gmail.com>
2018-07-14 16:00:04 +0100

    Visual overhaul
    I also updated all the champions

--- jssource.js
+++ jssource.js
@@ -1,67 +1,90 @@
 champions = {
-  "frontlines":
-    ["Ash",
+  "frontlines": [
+    "Ash",
     "Barik",
     "Fernando",
     "Inara",
+    "Khan",
     "Makoa",
     "Ruckus",
-    "Torvald"],
+    "Terminus",
+    "Torvald"
+  ],
 
-  "damage":
-    ["Bomb King",
+  "damage": [
+    "Bomb King",
     "Cassie",
     "Drogoz",
     "Kinessa",
+    "Lian",
     "Sha Lin",
+    "Strix",
     "Tyra",
     "Viktor",
-    "Strix",
-    "Lian",
-    "Willo"],
+    "Vivian",
+    "Willo"
+  ],
 
-  "support":
-    ["Grohk",
+  "support": [
+    "Furia",
+    "Grohk",
     "Grover",
+    "Jenos",
     "Mal'Damba",
     "Pip",
     "Seris",
-    "Ying",
-    "Jenos"],
+    "Ying"
+  ],
 
-  "flank":
-    ["Androxus",
+  "flank": [
+    "Androxus",
     "Buck",
     "Evie",
+    // "Koga",
     "Lex",
     "Maeve",
+    "Moji",
     "Skye",
-    "Zhin"],
+    "Talus",
+    "Zhin"
+  ],
 };
 
-function randomise(){
+function randomise() {
 
   pool = [];
 
-  $("input").each(function() {
+  $("input").each(function () {
 
-    if (this.checked){
+    if (this.checked) {
       pool = pool.concat(champions[this.id]);
     };
 
   });
 
   //choose a champion from the pool
-  chosen = pool[Math.floor(Math.random()*pool.length)];
-  //set the text to the correct champion
-  $("#answer").html(chosen);
+  chosen = pool[Math.floor(Math.random() * pool.length)];
+  console.log(chosen);
+  if (chosen === undefined) {
 
-  //setting the image
-  url = "imgs/120px-Champion_" + chosen.replace(/\s+/g, '') + "_Icon.png";
-  $("#profile").attr("src",url);
+    //Complain that no categories are checked
+    $("#answer").html("You need to select at least one category");
 
+    //Set the image to nothing
+    $("#profile").attr("src", "imgs/110px-Champion_None_Icon.png");
+
+  } else {
+
+    //set the text to the correct champion
+    $("#answer").html(chosen);
+
+    //setting the image
+    url = "imgs/110px-Champion_" + chosen.replace(/\s+/g, '') + "_Icon.png";
+    $("#profile").attr("src", url);
+
+  }
 };
 
-$( document ).ready(function() {
-    randomise();
+$(document).ready(function () {
+  randomise();
 });
