39b29af4e14e423a064b46fba50154b9794295d3
Allie <13716824+ChildishGiant@users.noreply.github.com>
2020-02-05 13:49:40 +0000

    Blacklist

--- jssource.js
+++ jssource.js
@@ -56,7 +56,53 @@ const champions = {
 	]
 };
 
-function randomise() {
+let blacklist = [];
+
+function toggleOverlay () {
+
+	const display =	document.getElementById("overlay").style.display;
+
+	document.getElementById("overlay").style.display = document.getElementById("overlay").style.display === "" ? "block" : "";
+}
+
+function getCookie (name) {
+	const value = `; ${document.cookie}`;
+	const parts = value.split(`; ${name}=`);
+	if (parts.length === 2) {
+		return parts.pop().split(";").shift();
+	}
+	return undefined;
+}
+
+
+function toggleBlacklist (el) {
+
+	if  (el.classList.contains("off")) {
+		el.classList.remove("off");
+
+		// Remove from blacklist
+		const index = blacklist.indexOf(el.id);
+		if (index > -1) {
+			blacklist.splice(index, 1);
+		}
+
+	}
+	else  {
+		el.classList.add("off");
+
+		// Add to blacklist
+		blacklist.push(el.id)
+	}
+
+	// Store blacklist as cookie
+
+	const string = JSON.stringify(blacklist)
+	document.cookie = `blacklist=${string}; path=/; max-age=31557600`
+
+	randomise();
+}
+
+function randomise () {
 
 	let pool = [];
 
@@ -67,8 +113,42 @@ function randomise() {
 		}
 	});
 
+	function isInBlacklist (x) {
+		return blacklist.indexOf(x) !== -1;
+	}
+
+	if (pool.length !== 0) {
+		// Check that not all of the pool are in the blacklist
+		if (pool.every(isInBlacklist)) {
+
+			// Complain that no champs could be chosen
+			document.getElementById("answer").innerHTML = "You've blacklisted all the champions you could roll";
+
+			// Set the image to nothing
+			document.getElementById("profile").src = "imgs/champs/none.jpg";
+
+			// Stop trying to pick a champ
+			return
+		}
+	}
+
+
+
 	// choose a champion from the pool
-	const chosen = pool[Math.floor(Math.random() * pool.length)];
+	let working = true;
+	let chosen;
+
+	while (working) {
+
+		// Choose a  random champion from the pool
+		chosen = pool[Math.floor(Math.random() * pool.length)];
+
+		// If the champion's not in the blacklist
+		if (blacklist.indexOf(chosen) === -1) {
+			working = false;
+		}
+	}
+
 	switch (chosen) {
 
 	case undefined:
@@ -91,5 +171,40 @@ function randomise() {
 }
 
 (function () {
+
+	// Load blacklist from cookies
+	blacklist = JSON.parse(getCookie("blacklist"));
+
 	randomise();
+
+	// Generate blacklist icons
+
+	// For category in champions
+	for (const cat in champions) {
+
+		// For champion in category
+		for (let index = 0; index < champions[cat].length; index++) {
+
+			const champ = champions[cat][index];
+			const template = document.querySelector("#template").cloneNode( true );
+
+			template.id = champ;
+
+			template.style.backgroundImage = `url(imgs/champs/${champ.toLowerCase().replace(" ", "-").replace("'", "\\'")}.jpg)`
+
+
+			template.children[0].src = `imgs/champs/${champ.toLowerCase().replace(" ", "-")}.jpg`;
+
+
+			// Check if disabled
+			if (blacklist.includes(champ)) {
+				template.classList.add("off");
+			}
+
+			document.getElementById(`${cat}-blacklist`).appendChild(template)
+
+		}
+	}
+
+
 }());
